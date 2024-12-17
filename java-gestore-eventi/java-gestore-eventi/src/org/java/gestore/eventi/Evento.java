a1ccbd0c89116bfc0727a563a965f8aecc9ded3d
claudia13-prog <cappellaclaudia13@gmail.com>
2024-12-17 19:50:51 +0100

    feat: added Evento class and its variables

---
+++ java-gestore-eventi/java-gestore-eventi/src/org/java/gestore/eventi/Evento.java
@@ -0,0 +1,18 @@
+package org.java.gestore.eventi;
+import java.time.LocalDate; // importo la classe LocalDate
+
+public class Evento {
+    
+    // variabili d'istanza
+    private String titolo;
+    private LocalDate data;
+    private int postiTotali;
+    private int postiPrenotati;
+
+
+
+
+
+
+
+}
