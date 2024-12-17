4c2b24f8158cfe29f0f23948da77740a6b423a7c
claudia13-prog <cappellaclaudia13@gmail.com>
2024-12-17 21:15:48 +0100

    feat: added controls to data and postiTotali

--- java-gestore-eventi/java-gestore-eventi/src/org/java/gestore/eventi/Evento.java
+++ java-gestore-eventi/java-gestore-eventi/src/org/java/gestore/eventi/Evento.java
@@ -12,14 +12,25 @@ public class Evento {
     // metodi
 
     // costruttore
-
     public Evento(String titolo, LocalDate data, int postiTotali){
+        
+        //controllo che la data non sia già passata
+        if(data.isBefore(LocalDate.now())){
+            System.out.println("Questa data è già passata");
+            return;  // se la data è passata esce dal costruttore
+        }
+
+        //controllo che il numero di posti totali sia positivo
+        if(postiTotali <= 0){
+            System.out.println("Il numero di posti deve essere positivo");
+            return; // se il numero di posti è negativo esce dal costruttore
+        }
+
         this.titolo = titolo;
-        this.data =data;
+        this.data = data;
         this.postiTotali = postiTotali;
         this.postiPrenotati = 0;
     }
-
 
 
 
