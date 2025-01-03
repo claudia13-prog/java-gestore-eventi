f2516bbd2d682d592b65be2ec356362811d36718
claudia13-prog <cappellaclaudia13@gmail.com>
2025-01-03 16:31:10 +0100

    feat: added aggiungiEvento method

--- java-gestore-eventi/java-gestore-eventi/src/org/java/gestore/eventi/ProgrammaEventi.java
+++ java-gestore-eventi/java-gestore-eventi/src/org/java/gestore/eventi/ProgrammaEventi.java
@@ -1,6 +1,8 @@
 package org.java.gestore.eventi;
 import java.util.List;
 import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class ProgrammaEventi {
     //attributi
@@ -14,10 +16,26 @@ public class ProgrammaEventi {
         this.titolo = titolo;
         this.eventi = new ArrayList<>();
     }
-    
+
+    // metodo che aggiunge alla lista un Evento, passato come parametro
+    public void aggiungiEvento(Evento evento){
+        this.eventi.add(evento);
+    }
+
+
+
+
+
     //prove
     public static void main(String[] args) {
-        ProgrammaEventi lista = new ProgrammaEventi("titolo");
-        System.out.println(lista);
+        ProgrammaEventi listaEventi = new ProgrammaEventi("Lista Eventi");
+        Evento concerto = new Concerto("AC/DC", LocalDate.of(2025, 07, 20), 300, LocalTime.of(21, 0), 80.00);
+        Evento compleanno = new Evento("compleanno", LocalDate.of(2025, 04, 13), 30);
+        Evento meeting = new Evento("Meeting di lavoro", LocalDate.of(2025, 07, 20), 30);
+
+        listaEventi.aggiungiEvento(concerto);
+        listaEventi.aggiungiEvento(compleanno);
+        listaEventi.aggiungiEvento(meeting);
+        System.out.println(listaEventi.eventi);
     }
 }
